08696d736241f4f3521e75a213a9cd798611362a
ezgiyamac <ezgiyamac@yahoo.com.tr>
2015-03-23 06:19:43 -0700

    empty templates

---
+++ iosAss/Assn_iphone_Ezgi/Assn_iphone_Ezgi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Assn_iphone_Ezgi
+//
+//  Created by Ezgi Yamac on 23/03/15.
+//  Copyright (c) 2015 Ezgi Yamac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
