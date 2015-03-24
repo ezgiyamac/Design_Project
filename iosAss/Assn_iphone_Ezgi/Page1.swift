567e2e6866edb21e23d3dafccd2e43a91df15c82
ezgiyamac <ezgiyamac@yahoo.com.tr>
2015-03-24 10:50:05 -0700

    Add Button Label and TextField, write some button actions

---
+++ iosAss/Assn_iphone_Ezgi/Page1.swift
@@ -0,0 +1,55 @@
+//
+//  Page1.swift
+//  Assn_iphone_Ezgi
+//
+//  Created by Ezgi Yamac on 24/03/15.
+//  Copyright (c) 2015 Ezgi Yamac. All rights reserved.
+//
+
+import UIKit
+
+class Page1: UIViewController {
+
+    @IBOutlet weak var Lbl1: UILabel!
+    
+    @IBOutlet weak var Lbl2: UILabel!
+    
+    @IBOutlet weak var Txt1: UITextField!
+    
+    @IBOutlet weak var Btn1: UIButton!
+    
+    var Count = 0
+    
+    
+    @IBAction func When_Pressed_Btn1(sender: AnyObject) {
+        
+        Lbl1.text = Txt1.text
+        
+        Count++
+        Lbl2.text = String(Count)
+    
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
